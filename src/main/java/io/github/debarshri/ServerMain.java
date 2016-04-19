deb8d836f4550a0e5daa66f8e84031be498f4f51
dbasak <dbasak@bol.com>
2016-04-19 23:23:06 +0200

    Almost the first release
    Change-Id: I382096d022ed0eb84675c369b83e216c135fc9ce

--- src/main/java/io/github/debarshri/ServerMain.java
+++ src/main/java/io/github/debarshri/ServerMain.java
@@ -18,19 +18,19 @@ public class ServerMain {
             Resource.LOCATION = "repo";
         }
 
-        get("/status", (req, res) -> "ok");
+        get("/v1/status", (req, res) -> "ok");
 
-        get("/", (req, res) -> "<pre>PropServe Doc\n\n" +
+        get("/v1", (req, res) -> "<pre>PropServe Doc\n\n" +
                 "/get/{property-name}\n" +
                 "/get/{property-name}?as={format}\n" +
                 "/get/{property-name}/field/{fieldName}</pre>\n");
 
-        get("/list", new ListAllProperties());
-        get("/get/:propertySetName", new PropertySet());
-        get("/get/:propertySetName/field/:propertyName", new NProperty());
-        post("/add/:propertyName", new CreateProperty());
-        post("/add/:propertyName/field/:fieldName/value", new AddValue());
-        get("/property/:propertyName/search/:fieldName", new Search());
+        get("/v1/list", new ListAllProperties());
+        get("/v1/get/:propertySetName", new PropertySet());
+        get("/v1//get/:propertySetName/field/:propertyName", new NProperty());
+        post("/v1/add/:propertyName", new CreateProperty());
+        post("/v1/add/:propertyName/field/:fieldName/value", new AddValue());
+        get("/v1/property/:propertyName/search/:fieldName", new Search());
 
         //todo search
     }
